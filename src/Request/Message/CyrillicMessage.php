3367c3e199616a6c8e09ff80bae8b99caf49d0d4
Michael Kumar <m.kumar@abv.bg>
2018-11-01 21:54:36 +0200

    added message validation/convertion with tests

--- src/Request/Message/CyrillicMessage.php
+++ src/Request/Message/CyrillicMessage.php
@@ -16,16 +16,33 @@ class CyrillicMessage implements Validator
 
     public function validate(&$data)
     {
-        // TODO: Implement validate() method.
+        $data = $this->clean($data);
+
+        if(strlen($data) > $this->messageLength)
+        {
+            return false;
+        }
+
+        return true;
     }
 
     public function clean($data)
     {
-        // TODO: Implement clean() method.
+        $data = $this->transliterate($data);
+
+        return $data;
     }
 
     private function transliterate($text)
     {
-        // TODO: Implement transliterate() method.
+        $cyr = array(
+            'ж',  'ч',  'щ',   'ш',  'ю',  'а', 'б', 'в', 'г', 'д', 'е', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ъ', 'ь', 'я',
+            'Ж',  'Ч',  'Щ',   'Ш',  'Ю',  'А', 'Б', 'В', 'Г', 'Д', 'Е', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ъ', 'Ь', 'Я');
+        $lat = array(
+            'zh', 'ch', 'sht', 'sh', 'yu', 'a', 'b', 'v', 'g', 'd', 'e', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'u', 'x', 'q',
+            'Zh', 'Ch', 'Sht', 'Sh', 'Yu', 'A', 'B', 'V', 'G', 'D', 'E', 'Z', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'c', 'U', 'X', 'Q');
+        if ($text) {
+            return str_replace($cyr, $lat, $text);
+        }
     }
 }
